5f7f588974611014ed13f07b2a853660409659a3
Timo Celik <timo.celik@gmail.com>
2021-03-11 02:40:57 +0100

    Add copy to clipboard functionality

--- src/App.js
+++ src/App.js
@@ -5,12 +5,23 @@ import umlautize from "./common/umlautize";
 function App() {
   const [ inputArea, setInputArea ] = useState("");
   const [ outputArea, setOutputArea ] = useState("");
+  const copyToClipboard = ({ currentTarget }) => {
+    const value = document.getElementById("outputfield");
+    value.select();
+    value.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+  }
   return (
     <div className="App">
       <form onSubmit={(e) => { e.preventDefault(); setOutputArea(umlautize(inputArea)); }}>
-        <TextArea className="textfield" id="inputfield" name="Input" value={inputArea} onChange={(e) => setInputArea(e.currentTarget.value)} />
-        <TextArea className="textfield" id="outputfield" name="Output" value={outputArea} onChange={(e) => setOutputArea(e.currentTarget.value)} />
-        <button type="submit" >Umlautize!</button>
+        <div className="row">
+          <TextArea className="textfield" id="inputfield" name="Input" value={inputArea} onChange={(e) => setInputArea(e.currentTarget.value)} />
+          <TextArea className="textfield" id="outputfield" name="Output" value={outputArea} onChange={(e) => setOutputArea(e.currentTarget.value)} />
+        </div>
+        <div className="row">
+          <button id="firstbtn" className="btn btn-primary" disabled={inputArea === "" && true} type="submit" >Umlautize!</button>
+          <button className="btn btn-secondary" disabled={outputArea === "" && true} type="button" onClick={copyToClipboard}>copy to clipboard</button>
+        </div>
       </form>
     </div>
   );
